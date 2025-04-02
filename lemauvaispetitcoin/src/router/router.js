b0a5ba23560c58ef0e77ecdc1a87f77beab32db4
clementAvesque <clement2avesque@gmail.com>
2025-04-02 10:14:53 +0200

    test du nouveau push

---
+++ lemauvaispetitcoin/src/router/router.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+// Importez vos composants ici
+
+
+const routes = [
+
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
---
+++ lemauvaispetitcoin/store/store.js
@@ -0,0 +1,10 @@
+import { defineStore } from 'pinia';
+
+export const useMainStore = defineStore('main', {
+  state: () => ({
+    // Ajoutez ici vos valeurs à stocker
+  }),
+  actions: {
+    // Ajoutez ici vos méthodes pour manipuler le state
+  },
+});
